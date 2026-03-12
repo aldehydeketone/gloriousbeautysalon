e5fedb35c99f866020566a149fcadb3d822f8520
Mihir Singh <mihir304singh@gmail.com>
2026-03-12 23:09:28 +0530

    fix vite multipage architecture

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        pricing: resolve(__dirname, 'pricing.html'),
+        notfound: resolve(__dirname, '404.html'),
+      },
+    },
+  },
+})
